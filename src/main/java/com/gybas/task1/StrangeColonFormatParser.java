19b83e5dfa85191ebdc72da7cfa639742d9b04b9
Stefan Gybas <extern.gybas_stefan@allianz.de>
2022-08-29 18:37:19 +0200

    - Make first test pass, add second test - Replace CharSequence with String - Update dependencies, mainly Java 11 to 17 - Replace underscores in display name of JUnit tests

--- src/main/java/com/gybas/task1/StrangeColonFormatParser.java
+++ src/main/java/com/gybas/task1/StrangeColonFormatParser.java
@@ -1,10 +1,11 @@
 package com.gybas.task1;
 
+import java.util.Collections;
 import java.util.Map;
 
 final class StrangeColonFormatParser {
 
-    Map<String, String> parse(CharSequence input) {
-        throw new UnsupportedOperationException();
+    Map<String, String> parse(String content) {
+        return Collections.emptyMap();
     }
 }
--- src/test/java/com/gybas/task1/StrangeColonFormatParserTests.java
+++ src/test/java/com/gybas/task1/StrangeColonFormatParserTests.java
@@ -1,17 +1,32 @@
 package com.gybas.task1;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 
+import static java.util.Map.entry;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayNameGeneration(ReplaceUnderscores.class)
 class StrangeColonFormatParserTests {
 
-    private StrangeColonFormatParser testee = new StrangeColonFormatParser();
+    private final StrangeColonFormatParser testee = new StrangeColonFormatParser();
 
     @Test
-    void returnEmptyMapForEmptyInput() {
+    void returns_an_empty_map_for_empty_content() {
         assertThat(testee.parse("")).isEmpty();
     }
+
+    @Test
+    void parses_a_single_line_without_spaces() {
+        //given
+        var content = "firstName:Tom";
+        var expectedEntry = entry("firstName", "Tom");
+
+        // when
+        var parsed = testee.parse(content);
+
+        // then
+        assertThat(parsed).containsOnly(expectedEntry);
+    }
 }
-
-
