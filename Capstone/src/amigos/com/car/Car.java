028f8fa8a6b3fe08ef2ecc6a5fcd13d6367b1541
Hugo Viana <extern.viana_hugo@allianz.de>
2024-08-09 10:24:16 +0100

    Move Capston project to AmigosCode

--- Capstone/src/amigos/com/car/Car.java
+++
@@ -1,6 +0,0 @@
-package amigos.com.car;
-
-import java.math.BigDecimal;
-
-public record Car(String regNumber, BigDecimal rentalPricePerDay, String brand, boolean isEletric) {
-}
--- Capstone/src/amigos/com/user/User.java
+++
@@ -1,9 +0,0 @@
-package amigos.com.user;
-
-import java.util.UUID;
-
-public record User(UUID uuid, String name) {
-    public User(String name){
-        this(UUID.randomUUID(),name);
-    }
-}
